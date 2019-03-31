712e5deffe0793007bb900689bdf91c76b3bd03b
Eszter Horvath <horvath.eszter.h@gmail.com>
2019-03-31 16:42:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name){
-  console.log ("Happy Holidays, ${name}")
+  return "Happy Holidays, ${name}!"
 }
 
 function happyHolidayTo(holiday,name){
